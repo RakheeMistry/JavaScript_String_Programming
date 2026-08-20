3a1a2f478d7a2a86ef500319462ddda195f5e98f
Rakhee <rsmistry05061996@gmail.com>
2026-08-20 14:36:39 +0530

    Rules of substring method of Javascript

--- Substring_method.js
+++ Substring_method.js
@@ -2,8 +2,11 @@
 const str = "World";
 console.log(str.substring(2,5));
 
-// Rule 2: If startIndex > endIndex
+// Rule 2: The Auto-Swap Rule (If startIndex > endIndex)
 console.log(str.substring(5,3)); // Swaps the parameters automatically to correct the order
 
-// Rule 3: Negative Values
-console.log(str.substring(-5)); // Treats negative numbers as 0
+// Rule 3: Negative Number(arguments) Rule
+console.log(str.substring(-5)); // Treats negative numbers or NAN as 0
+
+// Rule 4: The Equal Index Rule
+console.log(str.substring(4,4)); // If both indices are equal, the method returns an empty string ("")
