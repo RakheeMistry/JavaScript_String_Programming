2afffc8c90535da90f9aad88873fc508be6cf793
Rakhee <rsmistry05061996@gmail.com>
2026-08-29 23:32:54 +0530

    rules of indexOf_methods with string and array of javascript

--- IndexOf_methods.js
+++ IndexOf_methods.js
@@ -1,5 +1,5 @@
 // IndexOf() with string
-const str = "Typescript  20";
+const str = "Typescript  20 NaN";
 // Rule 1: First Match Wins: It performs a forward search and stops immediately at the first occurrence.
 console.log(str.indexOf('e'));
 
@@ -14,6 +14,15 @@ console.log(str.indexOf('t',-3));
 
 // Rule 4: Type Coercion: coerces non-string arguments into strings before searching.
 console.log(str.indexOf(20));
+console.log(str.indexOf(NaN));
 
 // Rule 5: Empty String Rule: In string searches, searching for an empty string "" with any fromIndex <= string.length returns that fromIndex.
 console.log(str.indexOf('',10));
+
+// IndexOf() with array
+const arr = ['apple', 'rat', 78, NaN];
+// Rule 1: First Match Wins: It performs a forward search and stops immediately at the first occurrence. 
+console.log(arr.indexOf('apple'));
+
+// Rule 4: Type Coercion: requires an exact type match (strict equality ===)
+console.log(arr.indexOf(NaN));
