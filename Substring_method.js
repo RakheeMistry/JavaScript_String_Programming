8c24e58b5b6ea22c59fe60c1a52edd658fb3d8d1
Rakhee <rsmistry05061996@gmail.com>
2026-09-08 23:56:10 +0530

    rule of Substring method with string of javascript

--- Substring_method.js
+++ Substring_method.js
@@ -6,10 +6,13 @@ console.log(str.substring(2,5));
 console.log(str.substring(5,3)); // Swaps the parameters automatically to correct the order
 
 // Rule 3: Negative Number(arguments) Rule
-console.log(str.substring(-5)); // Treats negative numbers or NAN as 0
-console.log(str.substring(-5,3)); // Treats negative numbers or NAN as 0
-console.log(str.substring(2,-3)); // Treats negative numbers or NAN as 0
-console.log(str.substring(-2,-3)); // Treats negative numbers or NAN as 0
+console.log(str.substring(-5)); // Treats NAN as 0
+console.log(str.substring(-5,3)); // Treats NAN as 0
+console.log(str.substring(2,-3)); // Treats NAN as 0
+console.log(str.substring(-2,-3)); // Treats NAN as 0
+
+//Rule 4: NaN(arguments) Rule
+console.log(str.substring(NaN,3)); // Treats NAN as 0
 
 // Rule 4: The Equal Index Rule
 console.log(str.substring(4,4)); // If both indices are equal, the method returns an empty string ("")
