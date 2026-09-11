a963bfd548718b725b7219ed3b85e8077e44533b
Rakhee <rsmistry05061996@gmail.com>
2026-09-11 12:19:19 +0530

    rules of substring method with string and array of javascript

--- Substring_method.js
+++ Substring_method.js
@@ -6,10 +6,10 @@ console.log(str.substring(2,5));
 console.log(str.substring(5,3)); // Swaps the parameters automatically to correct the order
 
 // Rule 3: Negative Number(arguments) Rule
-console.log(str.substring(-5)); // Treats NAN as 0
-console.log(str.substring(-5,3)); // Treats NAN as 0
-console.log(`Swap: ${str.substring(2,-3)}`); // Treats NAN as 0
-console.log(str.substring(-2,-3)); // Treats NAN as 0
+console.log(str.substring(-5)); // Treats Negative numbers as 0
+console.log(str.substring(-5,3)); // Treats Negative numbers as 0
+console.log(`Swap: ${str.substring(2,-3)}`); // Treats Negative numbers as 0
+console.log(str.substring(-2,-3)); // Treats Negative numbers as 0
 
 //Rule 4: NaN(arguments) Rule
 console.log(str.substring(NaN,3)); // Treats NAN as 0
@@ -20,3 +20,14 @@ console.log(str.substring(4,4)); // If both indices are equal, the method return
 
 // Rule 6: The Out of Range Rule
 console.log(str.substring(2,10)); // If endIndex is greater than the string length, it extracts upto the end of the string
+
+// Rule 7: Both Starting and Ending Index are decimal values
+console.log(str.substring(2.5,5.7)); // Decimal values are truncated to integers    
+
+// Rule 8: Both Starting and Ending Index are boolean values
+console.log(str.substring(true,false)); // Boolean values are converted to integers (1 and 0 respectively)
+
+// Rule 9: Both Starting and Ending Index are undefined
+// Note: Passing undefined as the start/end argument triggers default parameter
+console.log(str.substring(undefined,undefined)); // If both indices are undefined, the method returns the entire string 
+console.log(str.substring(undefined,3)); // If startIndex is undefined, it is treated as 0
